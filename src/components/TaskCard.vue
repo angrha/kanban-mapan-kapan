75cfa83a9818aa50472539b710a02521cc25501e
angrha <angrha@gmail.com>
2018-01-16 00:17:51 +0700

    remove modal, ongoing prev n next

--- src/components/TaskCard.vue
+++ src/components/TaskCard.vue
@@ -5,87 +5,89 @@
       <div class="card-body" style="text-align: left; margin: 0%">
         <p> Point : {{ task.point }} </p>
         <p> assigned to : {{ task.assignedto }} </p>
-        <button type="button" class="btn btn-success btn-sm" data-toggle="modal" data-target="#myModal" @click="detail">Show more</button>
-      </div>
-    </div>
-    <!-- modal -->
-    <div class="modal" id="myModal">
-      <div class="modal-dialog" role="document">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h5 class="modal-title"> Detail Task {{ titleDetail }}</h5>
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-              <span aria-hidden="true">&times;</span>
-            </button>
-          </div>
-          <div class="modal-body">
-            <ul class="list-group">
-              <li class="list-group-item d-flex justify-content-between align-items-center">
-                Description : {{ descDetail }}
-              </li>
-              <li class="list-group-item d-flex justify-content-between align-items-center">
-                Point : {{ pointDetail }}
-              </li>
-              <li class="list-group-item d-flex justify-content-between align-items-center">
-                Assigned to : {{ assignDetail }}
-              </li>
-            </ul>
-          </div>
-          <div class="modal-footer">
-            <button type="button" class="btn btn-primary" data-dismiss="modal" @click="removeTask">Delete</button>
-            <button type="button" class="btn btn-info">prev</button>
-            <button type="button" class="btn btn-info">next</button>
-          </div>
-        </div>
+        <!-- <button type="button" class="btn btn-success btn-sm" data-toggle="modal" data-target="#myModal" @click="detail">Show more</button> -->
+        <button type="button" class="btn btn-primary" @click="removeTask">Delete</button>
+        <button @click="prevMove(task)" type="button" class="btn btn-info">prev</button>
+        <button @click="nextMove(task)" type="button" class="btn btn-info">next</button>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import {todo, doing, done} from '@/firebase'
-// import {db} from '@/firebase'
+import {backlog, todo, doing, done} from '@/firebase'
 export default {
   props: ['task', 'db', 'index'],
   data () {
     return {
-      titleDetail: '',
-      descDetail: '',
-      pointDetail: 0,
-      assignDetail: ''
+      objDetail: {
+        assigned: '',
+        title: '',
+        point: 0,
+        description: ''
+      }
     }
   },
   methods: {
     removeTask () {
-      console.log(this.task)
       this.task.key = this.task['.key']
       this.task.db = this.db
       this.$emit('remove-task', this.task)
     },
-    detail () {
-      this.titleDetail = this.task.title
-      this.descDetail = this.task.description
-      this.pointDetail = this.task.point
-      this.assignDetail = this.task.assignedto
-      console.log(this.titleDetail)
-      console.log(this.task['.key'])
-    },
     nextMove (task) {
+      console.log('task', task.title)
+      console.log('ini dbdfdsdjfdsnjf', this.db)
+      let objDetail = {
+        assigned: task.assignedto,
+        title: task.title,
+        point: task.point,
+        description: task.description
+      }
+      console.log('ini obj', objDetail)
       switch (this.db) {
         case 'backlog' : {
-          todo.push(task)
+          todo.push(objDetail)
           break
         }
         case 'todo' : {
-          doing.push(task)
+          doing.push(objDetail)
           break
         }
         case 'doing' : {
-          done.push(task)
+          done.push(objDetail)
           break
         }
         case 'done' : {
-          done.push(task)
+          done.push(objDetail)
+          break
+        }
+      }
+    },
+    prevMove (task) {
+      console.log('task', task.title)
+      console.log('ini dbdfdsdjfdsnjf', this.db)
+      let objDetail = {
+        assigned: task.assignedto,
+        title: task.title,
+        point: task.point,
+        description: task.description
+      }
+      console.log('ini obj', objDetail)
+      switch (this.db) {
+        case 'backlog' : {
+          backlog.push(objDetail)
+          break
+        }
+        case 'todo' : {
+          backlog.push(objDetail)
+          break
+        }
+        case 'doing' : {
+          todo.push(objDetail)
+          break
+        }
+        case 'done' : {
+          todo.push(objDetail)
           break
         }
       }
